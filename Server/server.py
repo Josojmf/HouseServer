e419bb5d347538a9f139883ff4a9b93050e092c5
JosoJmf <you@example.com>
2022-06-28 16:27:41 +0200

    Firs commit

---
+++ Server/server.py
@@ -0,0 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        path = "../Files"+self.path
+        print(path)
+        message = f = open("../Files"+self.path, "r")
+        print(message)
+        self.wfile.write(bytes(message, "utf8"))
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        message = "Hello, World! Here is a POST response"
+        self.wfile.write(bytes(message, "utf8"))
+
+with HTTPServer(('', 8000), handler) as server:
+    server.serve_forever()
